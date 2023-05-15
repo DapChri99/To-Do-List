f74426ea2cf0e1633a6fed221cbc05480aae208c
DapChri99 <christiandapra08@gmail.com>
2023-05-15 18:15:31 +0200

    fix todo.js//wrapper

--- src/components/Todowrapper.js
+++ src/components/Todowrapper.js
@@ -1,6 +1,9 @@
 import React, {useState} from 'react'
 import {TodoForm} from './TodoForm'
 import { v4 as uuidv4 } from 'uuid'
+import { Todo } from './Todo'
+
+uuidv4()
 
 export const Todowrapper = () => {
     const [todos, setTodos] = useState([])
@@ -8,11 +11,16 @@ export const Todowrapper = () => {
     const addTodo = todo => {
         setTodos([...todos, {id: uuidv4(), task: todo,
             completed: false, isEditing: false}])
+
+        console.log(todos)
     }
   
  return (
     <div className='TodoWrapper'>
         <TodoForm addTodo={addTodo} />
+        {todos.map((todo, index) => (
+            <Todo task={todo} key={index} />
+         ))}
     </div>
   )
 }
