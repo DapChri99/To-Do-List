13e2021a62f4c2fc3771e174479e42cc318cebc0
DapChri99 <christiandapra08@gmail.com>
2023-05-15 18:30:55 +0200

    fix delete// edit task

--- src/components/Todowrapper.js
+++ src/components/Todowrapper.js
@@ -15,11 +15,28 @@ export const Todowrapper = () => {
         console.log(todos)
     }
   
+    const toggleComplete = id => {
+        setTodos(todos.map(todo => todo.id === id ? 
+            {...todo, completed: !todo.completed} : todo))
+    }
+
+    const deletTodo = id => {
+        setTodos(todos.filter(todo => todo.id !== id))
+    }
+
+    const editTodo = id => {
+        setTodos(todos.map(todo => todo.id === id ? 
+            {...todo, isEditing: !todo.isEditing} : todo))
+    }
  return (
     <div className='TodoWrapper'>
+        <h1>Get Things Done!</h1>
         <TodoForm addTodo={addTodo} />
         {todos.map((todo, index) => (
-            <Todo task={todo} key={index} />
+            <Todo task={todo} key={index}
+            toggleComplete={toggleComplete}
+            deletTodo= {deletTodo} 
+            editTodo={editTodo}/>
          ))}
     </div>
   )
